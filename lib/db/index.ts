afc1c2718039179e08ab03a85db59e6c1c3b5fcf
boben195 <boben195@ukr.net>
2025-05-27 17:27:02 +0300

    index +

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,14 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-import * as schema from "./schema"
+import * as schema from "./schema";
+import * as dotenv from "dotenv";
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("Missing DATABASE_URL in enviroment")
+}
+
+
 
 const sql = neon(process.env.DATABASE_URL!)
 

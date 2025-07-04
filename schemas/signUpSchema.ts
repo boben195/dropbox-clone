681fa0c35dd522690f3a9b82befb66d3cf5e32a2
boben195 <boben195@ukr.net>
2025-07-04 11:30:13 +0300

    add schema

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -8,7 +8,13 @@ export const signUpSchema = z.object({
         .email({ message: "Please enter valid email" }),
     password: z
         .string()
-        .min(1, {message: "Email is required"})
-    
-    
+        .min(1, { message: "Password is required" })
+        .min(8, { message: "Password shoud contain min 8 charachters" }),
+    passwordConformation: z
+        .string()
+        .min(1, { message: "Pleas confirm your passport" })
 })
+    .refine((data) => data.password === data.passwordConformation, {
+        message: "Password do not match",
+        path: ["passwordConformation"],
+    })
